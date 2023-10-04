c0c7ef29110abc15f17cf06cb3ebeb0d406f0d8e
jwywoo <jwywoo26@gmail.com>
2023-10-04 17:03:19 +0900

    init commit

---
+++ src/main/java/com/willyou/WillYouApplication.java
@@ -0,0 +1,13 @@
+package com.willyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WillYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WillYouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willyou/WillYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WillYouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
