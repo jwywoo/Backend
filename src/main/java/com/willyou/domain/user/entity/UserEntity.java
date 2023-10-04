2c97b9a6b6b20666e7143b1a1748736169b5a93f
DESKTOP-K1PBAQ0\win10 <kwe4248@gmail.com>
2023-10-04 21:58:54 +0900

    [#5]Feat: "Add login and signup"
    로그인 및 회원가입 API 개발
    
    Resolves: #5
    Ref: None
    Related to: None

---
+++ src/main/java/com/willyou/domain/user/entity/UserEntity.java
@@ -0,0 +1,33 @@
+package com.willyou.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+}
